b46213a81e908fa24066ffef9db4090adb81f3d9
Julie Cameron <jewlofthelotus@gmail.com>
2016-02-01 08:15:44 -0500

    revert google image upgrade

--- lita_config.rb
+++ lita_config.rb
@@ -26,7 +26,10 @@ Lita.configure do |config|
   # config.redis.host = "127.0.0.1"
   # config.redis.port = 1234
 
+  config.handlers.google_images.google_cse_id = ""
+  config.handlers.google_images.google_cse_key = ""
   config.handlers.google_images.safe_search = :medium
+
   config.handlers.giphy.api_key = "dc6zaTOxFJmzC"
 
   normalized_karma_user_term = ->(user_id, user_name) {
